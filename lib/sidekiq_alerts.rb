886518fa469bca9a9428e32eb924166209b36283
Danilo Dominguez <ddominguez@ubiqua.me>
2021-03-23 10:57:27 -0500

    Initial commit.

---
+++ lib/sidekiq_alerts.rb
@@ -0,0 +1,6 @@
+require "sidekiq_alerts/version"
+
+module SidekiqAlerts
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sidekiq_alerts/version.rb
@@ -0,0 +1,3 @@
+module SidekiqAlerts
+  VERSION = "0.1.0"
+end
