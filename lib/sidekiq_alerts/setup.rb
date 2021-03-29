61834252a7117f451d20d1d131ddf623bc0c10af
Danilo Dominguez <ddominguez@ubiqua.me>
2021-03-29 13:38:14 -0500

    Segregated sidekiq alerts functions and added Raven setup.

---
+++ lib/sidekiq_alerts/setup.rb
@@ -0,0 +1,13 @@
+Raven.configure do |config|
+  # URL where the events are sent
+  config.dsn = ENV['SENTRY_DSN_BACKEND']
+  # Filter fields filtered by Rails
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.ssl_verification = true
+  config.environments = %w[ production ]
+  # Set version, see application.html.haml
+  config.release = ENV['APP_RELEASE']
+end
+
+# Set Heroku App URL to event context
+Raven.extra_context heroku_app: ENV['APP_URL'] || ENV['APP_NAME']
